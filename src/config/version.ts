bf3b6119b4789c443d6c19d7bcfd518e519146f8
Edgars Jekabsons <edgars.jekabsons@gmail.com>
2026-01-18 14:38:41 +0200

    Bump version to 0.3.1

--- src/config/version.ts
+++ src/config/version.ts
@@ -1,5 +1,5 @@
 export const VERSION = {
-  number: process.env.VERSION || "0.1.3",
+  number: process.env.VERSION || "0.3.1",
   commit: process.env.COMMITHASH || "development",
   branch: process.env.BRANCH || "main",
   buildDate: process.env.BUILDDATE || "2025-04-30",
